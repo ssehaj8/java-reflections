2708a3a85c464d4fda2f7f9a1d1a16b77ca10a75
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-04-08 09:37:46 +0530

    [Sehajpreet] Add:class Calculator with a private method multiply(int a, int b). Use Reflection to invoke this method and display the result.

---
+++ src/main/java/com/bridgelabz/basicProblems/MethodInvoke.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.basicProblems;
+
+import java.lang.reflect.Method;
+
+class Calculator {
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
+public class MethodInvoke {
+    public static void main(String[] args) throws Exception {
+        Calculator calc = new Calculator();
+        Class<?> cls = calc.getClass();
+
+        Method method = cls.getDeclaredMethod("multiply", int.class, int.class);
+        method.setAccessible(true);
+
+        int result = (int) method.invoke(calc, 5, 10);
+        System.out.println("Result: " + result);
+
+    }
+}
+/*
+Result: 50
+ */
